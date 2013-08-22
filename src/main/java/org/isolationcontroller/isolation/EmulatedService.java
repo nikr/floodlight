16f2e68378b4756e0ea279eff07b8427bbcb65ab
Niklas Rehfeld <niklas.rehfeld@ecs.vuw.ac.nz>
2013-08-22 17:32:57 +1200

    modified EmulatedService to be maginally useful...

--- src/main/java/org/isolationcontroller/isolation/EmulatedService.java
+++ src/main/java/org/isolationcontroller/isolation/EmulatedService.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.isolationcontroller.isolation;
 
@@ -11,36 +11,47 @@ import org.openflow.protocol.action.OFAction;
 
 /**
  * This interface represents an emulated service on the isolated network.
- * 
+ *
  * @author Niklas Rehfeld
- * 
+ *
  */
 public interface EmulatedService
 {
+
+
+
+	public void setName(String name);
+	public String getName();
+
+
+	public void setDestinationIP(int IP);
+	public int getDestinationIP();
 
 	/**
 	 * Returns a OFFlowMod message that contains the actions to execute the
 	 * policy defined by this EmulatedService. Implementing classes should
 	 * return a valid OFFlowMod instance, that can be sent to the switch to
 	 * reroute the packets.
-	 * 
+	 *
 	 * @param in
 	 *            the packet that is to be rerouted.
 	 * @return an OFFlowMod message that implements this Emulated Service.
 	 */
-	public OFFlowMod getFlowMod();
-	
-	
+	//public OFFlowMod getFlowMod();
+
+
+	public void setActions(List<OFAction> actions);
+
 	/**
-	 * 
-	 * @return the actions that go in the FlowMod. 
+	 *
+	 * @return the actions that go in the FlowMod.
 	 */
 	public List<OFAction> getActions();
-	
+
 
 	/**
 	 * query whether the packet belongs to this service.
-	 * 
+	 *
 	 * @param in
 	 *            the packet that needs to be routed.
 	 * @return true if this emulated service is relevant to the incoming packet.
@@ -50,7 +61,7 @@ public interface EmulatedService
 	/**
 	 * Gets the priority of this rerouting. Lower priorities are processed
 	 * first.
-	 * 
+	 *
 	 * @return the priority for this service.
 	 */
 	// public int getPriority();
