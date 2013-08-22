5b75574815387fe6cf3c88a047513e87be93aa96
Niklas Rehfeld <niklas.rehfeld@ecs.vuw.ac.nz>
2013-08-22 14:51:36 +1200

    moved isolation controller to a different package.

---
+++ src/main/java/org/isolationcontroller/isolation/IsolationController.java
@@ -0,0 +1,245 @@
+/**
+ * 
+ */
+package org.isolationcontroller.isolation;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packetstreamer.thrift.OFMessageType;
+
+import org.openflow.protocol.OFFlowMod;
+import org.openflow.protocol.OFMatch;
+import org.openflow.protocol.OFMessage;
+import org.openflow.protocol.OFPacketIn;
+import org.openflow.protocol.OFType;
+import org.openflow.protocol.Wildcards;
+import org.openflow.protocol.Wildcards.Flag;
+import org.openflow.protocol.action.OFActionOutput;
+import org.openflow.util.HexString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author sand
+ * 
+ */
+public class IsolationController implements IOFMessageListener,
+		IFloodlightModule
+{
+
+	protected IFloodlightProviderService floodlightProvider;
+	protected static Logger logger;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.floodlightcontroller.core.IListener#getName()
+	 */
+	@Override
+	public String getName()
+	{
+		return IsolationController.class.getSimpleName();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.floodlightcontroller.core.IListener#isCallbackOrderingPrereq(java
+	 * .lang.Object, java.lang.String)
+	 */
+	@Override
+	public boolean isCallbackOrderingPrereq(OFType type, String name)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.floodlightcontroller.core.IListener#isCallbackOrderingPostreq(java
+	 * .lang.Object, java.lang.String)
+	 */
+	@Override
+	public boolean isCallbackOrderingPostreq(OFType type, String name)
+	{
+		// make sure we're before forwarding things.
+		return name.equalsIgnoreCase("forwarding");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.floodlightcontroller.core.module.IFloodlightModule#getModuleServices
+	 * ()
+	 */
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleServices()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.floodlightcontroller.core.module.IFloodlightModule#getServiceImpls()
+	 */
+	@Override
+	public Map<Class<? extends IFloodlightService>, IFloodlightService>
+			getServiceImpls()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.floodlightcontroller.core.module.IFloodlightModule#getModuleDependencies
+	 * ()
+	 */
+	@Override
+	public Collection<Class<? extends IFloodlightService>>
+			getModuleDependencies()
+	{
+		Collection<Class<? extends IFloodlightService>> deps =
+				new ArrayList<Class<? extends IFloodlightService>>();
+		// IfloodlightProviderService is for listening to OF messages.
+		deps.add(IFloodlightProviderService.class);
+		return deps;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.floodlightcontroller.core.module.IFloodlightModule#init(net.
+	 * floodlightcontroller.core.module.FloodlightModuleContext)
+	 */
+	@Override
+	public void init(FloodlightModuleContext context)
+			throws FloodlightModuleException
+	{
+		floodlightProvider =
+				context.getServiceImpl(IFloodlightProviderService.class);
+		logger = LoggerFactory.getLogger(IsolationController.class);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.floodlightcontroller.core.module.IFloodlightModule#startUp(net.
+	 * floodlightcontroller.core.module.FloodlightModuleContext)
+	 */
+	@Override
+	public void startUp(FloodlightModuleContext context)
+			throws FloodlightModuleException
+	{
+		// all other modules are initialised by now...
+		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.floodlightcontroller.core.IOFMessageListener#receive(net.
+	 * floodlightcontroller.core.IOFSwitch, org.openflow.protocol.OFMessage,
+	 * net.floodlightcontroller.core.FloodlightContext)
+	 */
+	@Override
+	public net.floodlightcontroller.core.IListener.Command receive(
+			IOFSwitch sw, OFMessage msg, FloodlightContext cntx)
+	{
+
+		Ethernet eth =
+				IFloodlightProviderService.bcStore.get(cntx,
+						IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+		// byte[] b = OFMessage.getData(sw, msg, cntx);
+
+		// if it's a PACKET_IN message we can check the incoming port.
+		OFPacketIn pktIn = null;
+		if (msg.getType() == OFType.PACKET_IN)
+		{
+			pktIn = (OFPacketIn) msg;
+			return blockPort(sw, pktIn, cntx);
+		}
+
+		// Long src = Ethernet.toLong(eth.getSourceMACAddress());
+		// Long dst = Ethernet.toLong(eth.getDestinationMACAddress());
+		// logger.info("Source: " + HexString.toHexString(src) + " Dest: "
+		// + HexString.toHexString(dst));
+		// if (src.longValue() == 1L)
+		// {
+		// logger.info("Dropping packet from source "
+		// + HexString.toHexString(src));
+		// return Command.STOP;
+		//
+		// }
+		return Command.CONTINUE;
+	}
+
+	/**
+	 * Adds a drop flow for the port that this packet came in on.
+	 * 
+	 * @param sw
+	 * @param pktIn
+	 * @param cntxt
+	 * @return
+	 */
+	private Command blockPort(IOFSwitch sw, OFPacketIn pktIn,
+			FloodlightContext cntxt)
+	{
+		short port = pktIn.getInPort();
+		if (sw.getId() == 2L && port == 1)
+		{
+			logger.info("Dropping packet from port " + sw.getStringId() + "/"
+					+ port);
+			OFFlowMod flow =
+					(OFFlowMod) floodlightProvider.getOFMessageFactory()
+							.getMessage(OFType.FLOW_MOD);
+			OFMatch m =
+					new OFMatch().setWildcards(Wildcards.FULL
+							.matchOn(Flag.IN_PORT));
+			m.setInputPort(port);
+			flow.setMatch(m);
+			flow.setCommand(OFFlowMod.OFPFC_ADD);
+			// flow.setActions()
+			flow.setActions(null);// no actions is equivalent to DROP according
+									// to s. 4.9 of OF spec.
+			// flow.setBufferId(sw.getBuffers()); //NOT SURE ABOUT THIS.
+			flow.setBufferId(pktIn.getBufferId());
+			try
+			{
+				logger.info("Writing flowmod: " + flow.toString());
+				sw.write(flow, cntxt);
+			}
+			catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return Command.STOP;
+	}
+
+}
